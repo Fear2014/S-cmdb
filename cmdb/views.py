09d3fdea7d7fbf25742b09fbfaf219d187a0dee8
wangkai <fear.wang2014@gmail.com>
2019-07-17 16:13:45 +0800

    add .gitattributes

--- cmdb/views.py
+++ cmdb/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import View,TemplateView,ListView
 from django.http import JsonResponse,HttpResponse,QueryDict
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.models import User
 from django.db.models import Q
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import *
