dcfa64a9f94cfb04882635b5e1a49abfad408823
wangkai <fear.wang2014@gmail.com>
2019-07-18 16:30:05 +0800

    优化分页样式

--- utils/baseview.py
+++ utils/baseview.py
@@ -6,17 +6,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class BaseListView(LoginRequiredMixin, ListView):
     model = None
     template_detial = None
-    paginator_numb = 10
+    paginator_numb = 2
 
     def handle_page(self, queryset, page):
         paginator = Paginator(queryset, self.paginator_numb, 0)
+        pagecount = paginator.num_pages
         try:
             paginator_data = paginator.page(page)
         except PageNotAnInteger:
             paginator_data = paginator.page(1)
         except EmptyPage:
             paginator_data = paginator.page(paginator.num_pages)
-        return paginator_data
+        data = {'paginator_data': paginator_data, 'page_count': pagecount}
+        return data
 
     def get (self, request, *args, **kwargs):
         pk = kwargs.get('pk')
@@ -28,5 +30,6 @@ class BaseListView(LoginRequiredMixin, ListView):
         #列表页
         queryset = self.get_queryset()
         search = self.request.GET.get('search', '')
-        paginator_data = self.handle_page(queryset, page)
-        return render(request, self.template_name, {'paginator_data': paginator_data, 'search': search})
+        paginator_data = self.handle_page(queryset, page).get('paginator_data')
+        page_count = self.handle_page(queryset, page).get('page_count')
+        return render(request, self.template_name, {'paginator_data': paginator_data, 'search': search, 'page_count': page_count})
